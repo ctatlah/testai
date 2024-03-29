f4601efe8b65eda883ed37d36f21079b7a3f3722
Charandeep Tatlah <ctatlah@tatlah-mac.local>
2024-03-29 12:01:50 -0700

    Build recommendation system for movie ratings

---
+++ aipractice/com/test/ai/utils/recommendUtils.py
@@ -0,0 +1,62 @@
+'''
+Created on Mar 29, 2024
+
+@author: ctatlah
+
+Utility methods that help with recommended system/colaberative filtering
+'''
+import numpy as np
+import tensorflow as tf
+
+def colabFilterCostFunc(X, W, b, Y, R, lambda_):
+    '''
+    Returns the cost for the content-based filtering
+    Args:
+      X (ndarray (num_movies,num_features)): matrix of item features
+      W (ndarray (num_users,num_features)) : matrix of user parameters
+      b (ndarray (1, num_users)            : vector of user parameters
+      Y (ndarray (num_movies,num_users)    : matrix of user ratings of movies
+      R (ndarray (num_movies,num_users)    : matrix, where R(i, j) = 1 if the i-th movies was rated by the j-th user
+      lambda_ (float): regularization parameter
+    Returns:
+      J (float) : Cost
+    '''
+    nm, nu = Y.shape # number of movies, number of users
+    J = 0
+    
+    for j in range(nu): # loop through users
+        for i in range(nm): # loop through movies
+            J += np.square(R[i,j] * (np.dot(X[i,:], W[j,:]) + b[0,j] - Y[i,j]))
+         
+    J = J/2   
+    J += (lambda_/2) * (np.sum(np.square(W)) + np.sum(np.square(X))) # regularize 
+    
+def colabFilterCostFuncFast(X, W, b, Y, R, lambda_):
+    '''
+    Returns the cost for the content-based filtering
+    Vectorized for speed. Uses tensorflow operations to be compatible with custom training loop.
+    Args:
+      X (ndarray (num_movies,num_features)): matrix of item features
+      W (ndarray (num_users,num_features)) : matrix of user parameters
+      b (ndarray (1, num_users)            : vector of user parameters
+      Y (ndarray (num_movies,num_users)    : matrix of user ratings of movies
+      R (ndarray (num_movies,num_users)    : matrix, where R(i, j) = 1 if the i-th movies was rated by the j-th user
+      lambda_ (float): regularization parameter
+    Returns:
+      J (float) : Cost
+    '''
+    j = (tf.linalg.matmul(X, tf.transpose(W)) + b - Y)*R
+    J = 0.5 * tf.reduce_sum(j**2) + (lambda_/2) * (tf.reduce_sum(X**2) + tf.reduce_sum(W**2))
+    return J
+
+def normalizeRatings(Y, R):
+    '''
+    Preprocess data by subtracting mean rating for every movie (every row).
+    Only include real ratings R(i,j)=1.
+    [Ynorm, Ymean] = normalizeRatings(Y, R) normalized Y so that each movie
+    has a rating of 0 on average. Unrated moves then have a mean rating (0)
+    Returns the mean rating in Ymean.
+    '''
+    Ymean = (np.sum(Y*R,axis=1)/(np.sum(R, axis=1)+1e-12)).reshape(-1,1)
+    Ynorm = Y - np.multiply(Ymean, R) 
+    return(Ynorm, Ymean)
